dfb832001a8d86df77acfae6e652531098790b03
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-22 11:38:18 -0500

    declare a new string type variable storytext to store the story text

--- lib/story.dart
+++ lib/story.dart
@@ -1,6 +1,6 @@
-//TODO: Step 2 - Create a new class called Story.
+// Step 2 - Create a new class called Story.
 class Story{
-
+String storyTitle;
 }
 
 //TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.

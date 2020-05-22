c14bc64008e73509314f6ea92b0f8ee48b57ac43
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-22 12:33:43 -0500

    Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,8 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    //Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                  storyBrain.nextStory(2);
                   },
                   color: Colors.blue,
                   child: Text(
